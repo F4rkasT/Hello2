5b09df4817fa47d36e464df56dab904713c743bb
2021293 <2021293@B006-14>
2022-02-04 09:41:51 +0100

    Add project files.

---
+++ Hello2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Konstansok
+            const string FEJLESZTO = "Farkas Tamás";
+            //Ez eg egysoros comment
+            /*
+             Ez többsoros
+            //Ez így jó
+             */
+            // Változók
+            string uzenet = "Helló Világ";
+            int szam = 10;
+            double szam2 = 10.3;
+            bool gender = true;
+            char karakter = 's';
+            string szoveg = "szöveg";
+
+            // Program törzse
+            Console.WriteLine(uzenet);
+            Console.WriteLine(szam);
+            Console.WriteLine(szam2);
+            Console.WriteLine(gender);
+            Console.WriteLine(karakter);
+            Console.WriteLine(szoveg);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Mtable22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtable22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = int.Parse(args[0]);
+            Console.WriteLine(szam * 2);
+            Console.ReadKey();
+        }
+    }
+}
