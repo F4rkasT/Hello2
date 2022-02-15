642b5ab8813b2d92261ddb477fe21bec84cc4eff
Tamás Farkas <2021293@dszcberegszaszi.hu>
2022-02-15 14:58:44 +0100

    nagyszam stb

---
+++ nagyszam/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nagyszam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "egyik";
+            string b = "másik";
+            int x = String.Compare(a, b);
+            if (x == 0)
+            {
+                Console.WriteLine("A két szám egyenlő");
+            }
+            else if (x < 0)
+            {
+                Console.WriteLine("Az 'a' a kisebb");
+            }
+            else
+            {
+                Console.WriteLine("Az 'a' a nagyobb");
+            }
+        }
+    }
+}
