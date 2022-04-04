43a4e6d9cf2cd0dea139d9b698d697069abd1500
Tamás Farkas <2021293@dszcberegszaszi.hu>
2022-04-04 13:01:02 +0200

    asd

---
+++ Sorozatok/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorozatok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+
+            Console.WriteLine("csá");
+            Console.WriteLine("4 elemet kérek");
+
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+            if (a - b == c - d & a - c == b - d)
+            {
+                Console.WriteLine("Ez egy Számtani");
+            }
+            else
+            {
+                if (a / b == c / d & b / c == b / d)
+                {
+                    Console.WriteLine("Ez egy Mértani");
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
