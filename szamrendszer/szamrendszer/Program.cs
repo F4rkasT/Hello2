27f2bc8d955e6b531a1884b2560a2bec1ad62d9a
F4rkasT <90308588+F4rkasT@users.noreply.github.com>
2022-04-05 00:02:03 +0200

    Add files via upload

---
+++ szamrendszer/szamrendszer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamrendszer
+{
+    class Szamrendszer
+    {
+        public Szamrendszer()
+        {
+
+        }
+
+        public void getSzamr()
+        {
+            Console.WriteLine("Hello!");
+
+            Console.WriteLine("Irj be egy számot!");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Milyen számrendszerbe váltsam");
+
+            int szam = Convert.ToInt32(Console.ReadLine());
+
+            string valtott = Convert.ToString(a, szam);
+
+            Console.WriteLine(valtott);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Szamrendszer szamrendszer = new Szamrendszer();
+            szamrendszer.getSzamr();
+            Console.ReadKey();
+        }
+    }
+}
