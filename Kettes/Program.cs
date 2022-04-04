598a26cc7923ec0cbdab856f4bbe156056960315
Tamás Farkas <2021293@dszcberegszaszi.hu>
2022-04-04 14:54:52 +0200

    asd

---
+++ Kettes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kettes
+{
+    class Kettes
+    {
+        private int szam;
+        private string valtott;
+        public Kettes() { }
+        public void valto()
+        {
+            Console.WriteLine("adja meg a számot");
+            szam = Convert.ToInt32(Console.ReadLine());
+            valtott = Convert.ToString(szam, 2);
+            Console.WriteLine("A szám");
+            Console.Write(valtott);
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Kettes a = new Kettes();
+                a.valto();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Lottó/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottó
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam;
+            Random rnd = new Random();
+            Console.WriteLine("Ötös lottó");
+            szam = Convert.ToInt32(Console.ReadLine());
+
+            int[] tomb = new int[5];
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = rnd.Next(1, 91);
+                for (int j = 0; j < i; j++)
+                {
+                    if (tomb[j] == tomb[i])
+                    {
+                        --i;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Nyerőszámok:");
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write("{0}", tomb[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
