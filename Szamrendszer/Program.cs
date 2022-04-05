bc874386b8d4628b08e737b1f42eb91162ecc6ec
Tamás Farkas <2021293@dszcberegszaszi.hu>
2022-04-05 13:53:25 +0200

    asd

---
+++ Szamrendszer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamrendszer
+{
+    class Szamrendszer
+    {
+        public Szamrendszer() { }
+
+        public void Szamr()
+        {
+            Console.WriteLine("Helló");
+
+            Console.WriteLine("Irj be egy számot");
+
+            int beirt = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Milyen számrendszerbe váltsam?");
+
+            int szamr = Convert.ToInt32(Console.ReadLine());
+
+            string valtott = Convert.ToString(beirt, szamr);
+
+            Console.WriteLine(valtott);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Szamrendszer szamrend = new Szamrendszer();
+            szamrend.Szamr();
+            Console.ReadKey();
+        }
+    }
+}
