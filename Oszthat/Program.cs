cfa0a102ba4b432409cbd8c31b97d446eac181a4
Tamás Farkas <2021293@dszcberegszaszi.hu>
2022-05-04 14:55:39 +0200

    asd

---
+++ Oszthat/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oszthat
+{
+    public class feladat
+    {
+        string bekérés;
+        int szam;
+        int a = 1;
+        public feladat() {}
+
+        public void setSzam()
+        {
+            while (this.a == 1)
+            {
+                Console.WriteLine("Számot kérek");
+                bekérés = Console.ReadLine();
+                this.a = 0;
+                
+            }
+          
+
+
+        }
+        public void getSzám()
+        {
+            if (szam % 3 == 0)
+            {
+                Console.WriteLine("Három");
+            }
+            {
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
