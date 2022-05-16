1c56f40872d6fbe28fa912d437847f0746f5187e
Tamás Farkas <2021293@dszcberegszaszi.hu>
2022-05-16 14:24:18 +0200

    d

---
+++ Lista_Kezelés/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Készitsünk oszályt városnevek tárolására
+Metódusok:
+setDb:bekérjük hogy hány nevet akarunk eltárolni. Ha semmit vagy 0-át ír be a felhasználó, akkor a beolvasást addig folytatjuk, amíg üres mezőt nem ír be.
+setNevek:Bekérjük a neveket az előbb megadott módon beolvassuk és hozzáadjuk a listához
+getNevek:kiiratjuk vesszővel elválasztva a letárolt neveket
+getVan:Bekérünk egy nevet és kiirjuk hogy szerepel-e a listában
+delNev: Bekérünk egy nevet.  Ha szerepel a listában, akkor töröljük, ha nem, akkor hozzáadjuk.
+delLast:kitörli a listában lévő utolsó elemet*/
+namespace Lista_Kezelés
+{
+    public class Városnevek 
+    {
+        int névdarab = 0;
+        string beolvas;
+        string név;
+        string folytatás;
+        List<string> lista = new List<string>();
+    public Városnevek() { }
+        public void setDb()
+        {
+            Console.WriteLine("Hány nevet akarunk eltárolni?");
+            névdarab = Convert.ToInt32(Console.ReadLine());
+            if (névdarab == 0)
+            {
+                
+            }
+        }
+        public void setNevek()
+        {
+            for (int i = 0; i < névdarab; i++)
+            {
+                Console.WriteLine("Kérek nevet");
+                név = Console.ReadLine();
+                lista.Add(beolvas);
+            }
+            
+        }
+        public void getNevek()
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine("{0},",lista[i]);
+            }
+            
+        }
+        public void getVan() 
+        {
+            Console.WriteLine("Kérek egy nevet");
+            bool hoppá = false;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (beolvas == lista[i])
+                {
+                    hoppá = true;
+                }
+                else { }
+            }
+            if (hoppá)
+            {
+                Console.WriteLine("Szerepel a listában");
+            }
+            else
+            {
+                Console.WriteLine("nem szerepel a listában");
+            }
+        }
+        public void delNev()
+        {
+            Console.WriteLine("kérek egy nevet");
+            beolvas = Console.ReadLine();
+            bool ezigen = false;
+            int azigen = 0;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (beolvas == lista[i])
+                {
+                    azigen = i;
+                    ezigen = true;
+                }
+            }
+            if (ezigen)
+            {
+                lista.RemoveAt(azigen);
+                Console.WriteLine("törölve lett");
+            }
+            else
+            {
+                lista.Add(beolvas);
+                Console.WriteLine("Hozzáadva");
+            }
+
+        }
+        public void delLast()
+        {
+            lista.RemoveAt(lista.Count - 1);
+            Console.WriteLine("Utolsó elem törölve");
+        }
+    }
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Városnevek vege = new Városnevek();
+            vege.setDb();
+            vege.setNevek();
+            vege.getNevek();
+            vege.getVan();
+            vege.delNev();
+            vege.delLast();
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ henger/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("henger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("henger")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f9baedd2-4965-4aaf-abb5-b48a82be1fc2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
